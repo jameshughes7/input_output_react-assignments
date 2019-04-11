687197bef3f87c40832e6562793df54c4067e990
jameshughes7 <hugjimbo77@gmail.com>
2019-04-11 10:10:09 +0100

    added UserInput and UserOutput components and rendering multiple UserOutput components

---
+++ src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const userOutput = () => {
+        return (
+            <div>
+                <p>This is the 1st paragraph</p>
+                <p>This is the 2nd paragraph</p>
+            </div>
+        )
+    }
+
+export default userOutput;
