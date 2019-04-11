0a3907ce256d94d387fd22089775ed4b0b0005ba
jameshughes7 <hugjimbo77@gmail.com>
2019-04-11 10:55:09 +0100

    styled input and output components

--- src/UserInput/UserInput.js
+++ src/UserInput/UserInput.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
 const userInput = (props) => {
+    const inputStyle ={
+        border: "2px solid purple",
+        backgroundColor: 'yellow',
+        cursor: 'pointer',
+    }
         return (
-            <input type='text' 
+            <input 
+            type='text'
+            style={inputStyle}
             onChange={props.changed} 
             value={props.currentName}/>
         )
